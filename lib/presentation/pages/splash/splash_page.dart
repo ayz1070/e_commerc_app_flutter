a6e64c3ccc40133d9bc36bf3752a88c479db6469
Jun <ayz1070@gmail.com>
2024-05-18 20:10:25 +0900

    [FEAT #2] 스플래시 화면 구현

--- lib/presentation/pages/splash/splash_page.dart
+++ lib/presentation/pages/splash/splash_page.dart
@@ -1,5 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../routes/route_path.dart';
+
+
+// TODO 로그인 bloc
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
@@ -8,18 +16,22 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    // go : 스택에 쌓이지 않음
+    // push : 스택에 쌓임
+    Timer(Duration(seconds:2), () => context.go(RoutePath.home));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("splash"),
-      ),
+      // TODO 하드코딩된 값 변경할 것
+      backgroundColor: Color(0xFF5F0080),
       body: Center(
-        child: Text(
-          'splash_page',
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
-      ),
+        child: SvgPicture.asset('assets/svg/main_logo.svg'),
+      )
     );
   }
 }
