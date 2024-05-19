447e273f7deaa73f31af88e22c032ba580501cab
Jun <ayz1070@gmail.com>
2024-05-19 22:49:59 +0900

    [FEAT] BottomNavigation 및 Cubit 개발

---
+++ lib/presentation/main/cubit/bottom_nav_cubit.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../core/theme/constant/app_icons.dart';
+
+enum BottomNav {
+  home, category, search, user
+}
+
+class BottomNavCubit extends Cubit<BottomNav>{
+  BottomNavCubit() : super(BottomNav.home);
+
+  // 바텀 네비게이션의 누른 버튼의 인덱스 값을 방출
+  void changeIndex(int index)=> emit(BottomNav.values[index]);
+}
+
+// extension 은 기존 클래스를 확장하는 역할의 키워드
+extension BottomNavX on BottomNav{
+  String get icon {
+    switch(this){
+      case BottomNav.home:
+        return AppIcons.navHome;
+      case BottomNav.category:
+        return AppIcons.navCategory;
+      case BottomNav.search:
+        return AppIcons.navSearch;
+      case BottomNav.user:
+        return AppIcons.navUser;
+    }
+  }
+
+  String get activeIcon{
+    switch(this){
+      case BottomNav.home:
+        return AppIcons.navHomeOn;
+      case BottomNav.category:
+        return AppIcons.navCategoryOn;
+      case BottomNav.search:
+        return AppIcons.navSearchOn;
+      case BottomNav.user:
+        return AppIcons.navUserOn;
+    }
+  }
+
+  String get toName{
+    switch(this){
+      case BottomNav.home:
+        return "홈";
+      case BottomNav.category:
+        return "카테고리";
+      case BottomNav.search:
+        return "검색";
+      case BottomNav.user:
+        return "마이페이지";
+    }
+  }
+}
